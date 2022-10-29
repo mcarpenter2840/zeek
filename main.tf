1c782c3f0a0474c61e80854a955fbd5aa6366b62
Marvin Carpenter <marvin.carpenter@gmail.com>
2022-10-29 14:17:51 -0500

    imported resource

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "zeek" {
+  ami           = "ami-097a2df4ac947655f"
+  instance_type = "t2.medium"
+}
